ed71d8d6cfe4beb5f3a8f46e212a73d435af6afc
Luke <druby-luke@users.noreply.github.com>
2018-03-05 15:03:02 +0100

    Add files via upload
    buttons

---
+++ Pause.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VangdeVolger
+{
+    class Pause : Button
+    {
+        void GamePause()
+        {
+
+        }
+        void GameResume()
+        {
+
+        }
+
+    }
+}

ec215c04f98efbec4c5ad94e8db0f4750f1535ba
Jordan <SubwoofGaming@gmail.com>
2018-03-13 15:35:55 +0100

    GameField

---
+++ VangdeVolger/GameField.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VangdeVolger
+{
+    class GameField
+    {
+        // Up, Right, Down, Left.
+        public GameObject[] neighboor;
+        // The object it holds.
+        public GameObject contains;
+    }
+}
